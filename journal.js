59da806c4962f7a21817ea7e2b4ac82d9683fd62
Isaac Garcia <garcia_isaac01@student.smc.edu>
2026-07-01 00:26:11 -0700

    Add farewell() function to journal

--- journal.js
+++ journal.js
@@ -1,5 +1,10 @@
 // Greet a user by name
-
 function greet(name){
     return "Hello, " + name + "!"
 }
+
+// Say goodbye to user by name
+function farewell(name){
+    return "Bye-bye, " + name + ", until next time."
+}
+
